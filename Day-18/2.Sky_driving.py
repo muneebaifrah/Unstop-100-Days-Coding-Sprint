0dba072e1e9caf27e76f4a4d11f46fcfe0ce9214
muneebaifrah <muneebaifrah08@gmail.com>
2026-03-04 12:38:52 +0530

    M

--- Day-18/2.Sky_driving.py
+++ Day-18/2.Sky_driving.py
@@ -1,18 +1,20 @@
 def min_parachutes(k, n):
-    if n == 0 or n == 1:
-        return n
+    # Base cases
+    if n == 0:
+        return 0
     if k == 0:
         return 0
-    
-    # dp[j] will represent dp[m][j]
+    if k == 1:
+        return n
+
     dp = [0] * (k + 1)
     moves = 0
-    
+
     while dp[k] < n:
         moves += 1
         for j in range(k, 0, -1):
             dp[j] = dp[j] + dp[j-1] + 1
-    
+
     return moves
 
 
