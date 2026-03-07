c3135bc3041a2dbd111c03b9a5a1162830652348
muneebaifrah <muneebaifrah08@gmail.com>
2026-03-07 14:07:50 +0530

    M

---
+++ Day-30/1.The_Hint.py
@@ -0,0 +1,20 @@
+def LongestConsecutiveCharacter(s):
+    max_count = 1
+    current_count = 1
+    
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            current_count += 1
+        else:
+            max_count = max(max_count, current_count)
+            current_count = 1
+    
+    max_count = max(max_count, current_count)
+    
+    return max_count
+
+
+# Input
+s = input().strip()
+
+print(LongestConsecutiveCharacter(s))
