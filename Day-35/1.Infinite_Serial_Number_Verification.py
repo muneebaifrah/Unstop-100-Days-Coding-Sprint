05db68aa783303bdb9cd694ea30d1c6d2bc8820a
muneebaifrah <muneebaifrah08@gmail.com>
2026-03-08 16:49:19 +0530

    M

---
+++ Day-35/1.Infinite_Serial_Number_Verification.py
@@ -0,0 +1,15 @@
+def find_digit_at_position(N):
+    length = 1
+    count = 9
+    start = 1
+
+    while N > length * count:
+        N -= length * count
+        length += 1
+        count *= 10
+        start *= 10
+
+    number = start + (N - 1) // length
+    digit_index = (N - 1) % length
+
+    return str(number)[digit_index]
