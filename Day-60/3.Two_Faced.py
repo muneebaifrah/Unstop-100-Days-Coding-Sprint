9eb0da2341094b6057b85e53cec6b2d2a497bd1d
muneebaifrah <muneebaifrah08@gmail.com>
2026-03-14 11:25:39 +0530

    M

--- Day-60/3.Two_Faced.py
+++ Day-60/3.Two_Faced.py
@@ -1,38 +1,19 @@
-def user_logic(n, P, Q):
-    MOD = 998244353
+MOD = 998244353
 
-    # Build permutation f where f[P[i]] = Q[i]
-    f = [0] * (n + 1)
-    for i in range(n):
-        f[P[i]] = Q[i]
+def count_valid_ways(n, P, Q):
+    total_ways = 0
+    for mask in range(1, 1 << n):
+        covered = set()
+        for i in range(n):
+            if mask & (1 << i):
+                covered.add(P[i])
+                covered.add(Q[i])
+        if len(covered) == n:
+            total_ways = (total_ways + 1) % MOD
+    return total_ways
 
-    visited = [False] * (n + 1)
-    ans = 1
+n = int(input())
+P = list(map(int, input().split()))
+Q = list(map(int, input().split()))
 
-    for i in range(1, n + 1):
-        if not visited[i]:
-            cur = i
-            length = 0
-
-            while not visited[cur]:
-                visited[cur] = True
-                cur = f[cur]
-                length += 1
-
-            if length == 1:
-                ans = (ans * 1) % MOD
-            else:
-                ans = (ans * (pow(2, length, MOD) - 2)) % MOD
-
-    return ans % MOD
-
-
-if __name__ == "__main__":
-    import sys
-    data = sys.stdin.read().split()
-
-    n = int(data[0])
-    P = list(map(int, data[1:1 + n]))
-    Q = list(map(int, data[1 + n:1 + 2 * n]))
-
-    print(user_logic(n, P, Q))
+print(count_valid_ways(n, P, Q))
